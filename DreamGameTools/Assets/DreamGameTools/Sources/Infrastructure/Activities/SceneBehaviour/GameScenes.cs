cff6c92828fb360bc01097c843d583f2c84db0c9
Vladislav <singayevskyvladislav@gmail.com>
2022-05-11 19:18:03 +0300

    Added game scenes config

---
+++ DreamGameTools/Assets/DreamGameTools/Sources/Infrastructure/Activities/SceneBehaviour/GameScenes.cs
@@ -0,0 +1,8 @@
+namespace DreamGameTools.Client
+{
+    public static class GameScenes
+    {
+        public const string Bootstrap = "Bootstrap";
+        public const string Sandbox = "Sandbox";
+    }
+}
